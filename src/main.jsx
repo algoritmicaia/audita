8405124abda0a31df814089a1edddb25750dbb3d
SaidJacobo <SaidJacobo@outlook.com>
2025-09-07 18:12:02 -0300

    Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import IluminationForm from './components/Pages/IluminationPage/IluminationPage'
+import IluminationPage from './components/Pages/IluminationPage/IluminationPage'
 import { BrowserRouter } from "react-router";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCaretUp, faMinus } from '@fortawesome/free-solid-svg-icons'
-import { Analytics } from "@vercel/analytics/next"
+
 // Configurar los iconos que vamos a usar
 library.add(faCaretUp, faMinus)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <IluminationForm />
+      <IluminationPage />
     </BrowserRouter>
   </StrictMode>,
 )
