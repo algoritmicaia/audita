611d44137591cd471a25cda96a9edb6646fa58bc
SaidJacobo <SaidJacobo@outlook.com>
2025-09-07 18:04:55 -0300

    react analytics

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import IluminationForm from './components/Pages/IluminationPage/IluminationPage'
 import { BrowserRouter } from "react-router";
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faCaretUp, faMinus } from '@fortawesome/free-solid-svg-icons'
-
+import { Analytics } from "@vercel/analytics/next"
 // Configurar los iconos que vamos a usar
 library.add(faCaretUp, faMinus)
 
