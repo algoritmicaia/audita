c9852efc4d41d479d1b9ab8429c132445bd72b34
SaidJacobo <saidjacobo@outlook.com>
2025-09-22 14:12:19 -0300

    mas ajustecillos

--- src/components/Ui/Header.jsx
+++ src/components/Ui/Header.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <header className="bg-white py-4">
+    <header className="bg-white">
       <div className="flex items-center justify-between">
         <Link to="/" className="flex items-center gap-2 hover:opacity-80 transition-opacity">
           <img 
             src="/audita.png" 
             alt="audita" 
+            className="h-10 w-auto"
           />
         </Link>
         <div className="flex items-center space-x-4">
